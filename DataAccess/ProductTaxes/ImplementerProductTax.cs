560cb6d10ac0b647173c8e31900e722ecdde15d1
Rafael Villalobos <rvilla3452@gmail.com>
2020-07-08 23:34:37 -0600

    Implementacion ProductTax en DA

---
+++ DataAccess/ProductTaxes/ImplementerProductTax.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.ProductTaxes
+{
+    public class ImplementerProductTax
+    {
+        #region Metodos
+        public ObjectResponse<bool> Insert(ProductTax productTax)
+        {
+            return Repository.Insert(productTax);
+        }
+
+        public ObjectResponse<bool> Update(ProductTax productTax)
+        {
+            return Repository.Update(productTax);
+        }
+
+        public ObjectResponse<bool> Delete(int productTaxId)
+        {
+            return Repository.Delete(productTaxId);
+        }
+
+        public ObjectResponse<ProductTax> Get(int productTaxId)
+        {
+            return Repository.Get(productTaxId);
+        }
+
+        public ObjectResponse<IEnumerable<ProductTax>> GetAll(bool deleteItems)
+        {
+            var productTaxes = Repository.GetAll();
+
+            if (!productTaxes.IsSuccess)
+                return productTaxes;
+
+            if (!deleteItems)
+                productTaxes.Data = productTaxes.Data.ToList().Where(x => !x.Delete).ToList();
+
+            return productTaxes;
+        }
+
+        #endregion
+    }
+}
---
+++ DataAccess/ProductTaxes/Repository.cs
@@ -0,0 +1,69 @@
+﻿using Common;
+using Common.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.ProductTaxes
+{
+    public class Repository
+    {
+        #region Metodos
+        public static ObjectResponse<bool> Insert(ProductTax productTax)
+        {
+            using (var db = new DataContext())
+            {
+                db.ProductTaxes.Add(productTax);
+                db.SaveChanges();
+                return new ObjectResponse<bool>(true, "Impuesto relacionado");
+            }
+
+        }
+
+        public static ObjectResponse<bool> Update(ProductTax productTax)
+        {
+            using (var db = new DataContext())
+            {
+                db.Entry(productTax).State = EntityState.Modified;
+                db.SaveChanges();
+                return new ObjectResponse<bool>(true, "Relacion actualizada");
+            }
+        }
+
+        public static ObjectResponse<bool> Delete(int productTaxId)
+        {
+            using (var db = new DataContext())
+            {
+                var productTax = db.ProductTaxes.Find(productTaxId);
+                if (productTax == null)
+                    return new ObjectResponse<bool>(false, "No se encontro el impuesto relacionado");
+                productTax.Delete = true;
+                db.Entry(productTax).State = EntityState.Modified;
+                db.SaveChanges();
+                return new ObjectResponse<bool>(true, "Relacion eliminada");
+            }
+        }
+
+        public static ObjectResponse<ProductTax> Get(int productTaxId)
+        {
+            using (var db = new DataContext())
+            {
+                var productTax = db.ProductTaxes.Find(productTaxId);
+                return new ObjectResponse<ProductCategory>(true, "Consulta exitosa", productTax);
+            }
+        }
+
+        public static ObjectResponse<IEnumerable<ProductTax>> GetAll()
+        {
+            using (var db = new DataContext())
+            {
+                var productTaxes = db.ProductTaxes.ToList();
+                return new ObjectResponse<IEnumerable<ProductTax>>(true, "Consulta exitosa", productTaxes);
+            }
+        }
+
+        #endregion
+    }
+}
